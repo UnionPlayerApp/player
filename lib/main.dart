c05eb0a19157108ec07674fd49bcb02b50b65df7
Alexander Chepel <chepel.alexander@gmail.com>
2021-04-14 18:54:02 +0300

    UP-3: add: BloC library is implemented into main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:audio_session/audio_session.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:logger/logger.dart';
-
-const LOG_TAG = "UPA -> ";
-const STREAM_URL = "http://78.155.222.238:8010/souz_radio";
-
-late AudioPlayer _player = AudioPlayer();
-late Logger logger = Logger();
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:union_player_app/screen_main/main_bloc.dart';
+import 'package:union_player_app/screen_main/main_event.dart';
+import 'package:union_player_app/screen_main/main_state.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,145 +18,64 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: MainPage(title: 'Union Player Home Page'),
+      home: BlocProvider(
+          create: (context) => MainBloc(),
+          child: MainPage(title: 'Union Player Home Page')
+      ),
     );
   }
 }
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   MainPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+  late final MainBloc mainBloc;
 
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  final _source = AudioSource.uri(Uri.parse(STREAM_URL));
-
-  String _stateString01 = "Play / Pause";
-  String _stateString02 = "Loading / Buffering / Completed / Ready ";
-  IconData _fabIconData = Icons.play_arrow_rounded;
-
-  void _playerStartStop() {
-    if (_player.processingState == ProcessingState.ready) {
-      if (_player.playing) {
-        _player.pause();
-      } else {
-        _player.play();
-      }
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    initPlayer();
-  }
-
-  Future<void> initPlayer() async {
-    final session = await AudioSession.instance;
-    await session.configure(AudioSessionConfiguration.music());
-
-    _player.playerStateStream.listen((state) {
-      if (state.playing) {
-        _setStateToPlay();
-      } else {
-        _setStateToPause();
-      }
-      switch (state.processingState) {
-      case ProcessingState.idle:
-        _setStateString02("Idle");
-        break;
-      case ProcessingState.loading:
-        _setStateString02("Loading");
-        break;
-      case ProcessingState.buffering:
-        _setStateString02("Buffering");
-        break;
-      case ProcessingState.ready:
-        _setStateString02("Ready");
-        break;
-      case ProcessingState.completed:
-        _setStateString02("Completed");
-        break;
-      }
-    });
-
-    _player.playbackEventStream.listen((event) {},
-        onError: (Object e, StackTrace stackTrace) {
-      showError("A stream error occurred", e);
-    });
-    try {
-      await _player.setAudioSource(_source);
-    } catch (e) {
-      showError("Stream load error happens", e);
-    }
-  }
-
-  void _setStateToPlay() {
-    setState(() {
-      _stateString01 = "Playing";
-      _fabIconData = Icons.pause_rounded;
-    });
-  }
-
-  void _setStateToPause() {
-    setState(() {
-      _stateString01 = "Pausing";
-      _fabIconData = Icons.play_arrow_rounded;
-    });
-  }
-
-  void _setStateString02(String text) {
-    setState(() {
-      _stateString02 = text;
-    });
-  }
-
-  @override
-  void dispose() {
-    _player.dispose();
-    super.dispose();
-  }
-
-  AppBar createAppBar() => AppBar(
-        title: Text(widget.title),
+  AppBar createAppBar() =>
+      AppBar(
+        title: Text(title),
       );
 
-  Text createStateRow1() => Text(
-      _stateString01,
-      style: Theme.of(context).textTheme.bodyText2,
-    );
-
-  Text createStateRow2() => Text(
-        _stateString02,
-        style: Theme.of(context).textTheme.bodyText2,
+  Text createStateRow(BuildContext context, String stateStr) =>
+      Text(
+        stateStr,
+        style: Theme
+            .of(context)
+            .textTheme
+            .bodyText2,
       );
 
-  FloatingActionButton createFAB() => FloatingActionButton(
-        onPressed: _playerStartStop,
+  FloatingActionButton createFAB(IconData iconData) =>
+      FloatingActionButton(
+        onPressed: () => mainBloc.add(PlayPauseFabPressed()),
         tooltip: 'Play / Pause',
-        child: Icon(_fabIconData),
+        child: Icon(iconData),
       );
 
   @override
   Widget build(BuildContext context) {
+    mainBloc = BlocProvider.of<MainBloc>(context);
     return Scaffold(
         appBar: createAppBar(),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[createStateRow1(), createStateRow2()],
+          child: BlocBuilder<MainBloc, MainState>(
+            builder: (_, mainState) =>
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    createStateRow(context, mainState.stateStr01),
+                    createStateRow(context, mainState.stateStr02)
+                  ],
+                ),
           ),
         ),
-        floatingActionButton:
-            createFAB() // This trailing comma makes auto-formatting nicer for build methods.
-        );
-  }
-
-  void showError(String msg, Object error) {
-    logger.d("$LOG_TAG $msg: $error");
+        floatingActionButton: BlocBuilder<MainBloc, MainState>(
+            buildWhen: (oldState, newState) =>
+              oldState.stateStr01 != newState.stateStr01,
+            builder: (_, mainState) =>
+              createFAB(mainState.iconData)
+        )
+    );
   }
 }
--- lib/screen_main/main_data.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MainData {
-  String? stateStr01;
-  String? stateStr02;
-  IconData? iconData;
-}
