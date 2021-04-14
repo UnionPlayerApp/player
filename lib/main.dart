2d326e38ad6a334afe6645523ed49a08d7c669bd
chplalex <chepel.alexander@gmail.com>
2021-04-14 21:50:45 +0300

    UP-3: Move main page code into separate file

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:union_player_app/screen_main/main_bloc.dart';
-import 'package:union_player_app/screen_main/main_event.dart';
-import 'package:union_player_app/screen_main/main_state.dart';
+import 'package:union_player_app/screen_main/main_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,60 +21,6 @@ class MyApp extends StatelessWidget {
           create: (context) => MainBloc(),
           child: MainPage(title: 'Union Player Home Page')
       ),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  MainPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-  late final MainBloc mainBloc;
-
-  AppBar createAppBar() =>
-      AppBar(
-        title: Text(title),
-      );
-
-  Text createStateRow(BuildContext context, String stateStr) =>
-      Text(
-        stateStr,
-        style: Theme
-            .of(context)
-            .textTheme
-            .bodyText2,
-      );
-
-  FloatingActionButton createFAB(IconData iconData) =>
-      FloatingActionButton(
-        onPressed: () => mainBloc.add(PlayPauseFabPressed()),
-        tooltip: 'Play / Pause',
-        child: Icon(iconData),
-      );
-
-  @override
-  Widget build(BuildContext context) {
-    mainBloc = BlocProvider.of<MainBloc>(context);
-    return Scaffold(
-        appBar: createAppBar(),
-        body: Center(
-          child: BlocBuilder<MainBloc, MainState>(
-            builder: (_, mainState) =>
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    createStateRow(context, mainState.stateStr01),
-                    createStateRow(context, mainState.stateStr02)
-                  ],
-                ),
-          ),
-        ),
-        floatingActionButton: BlocBuilder<MainBloc, MainState>(
-            buildWhen: (oldState, newState) =>
-              oldState.stateStr01 != newState.stateStr01,
-            builder: (_, mainState) =>
-              createFAB(mainState.iconData)
-        )
     );
   }
 }
