6ef164143cfa5e659d6ad4eef4647c74c0a9d723
chplalex <chepel.alexander@gmail.com>
2021-05-07 10:17:36 +0300

    UP-14 Codestyle refactoring.

--- lib/screen_feedback/feedback_page.dart
+++ lib/screen_feedback/feedback_page.dart
@@ -1,17 +1,16 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:union_player_app/utils/app_logger.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:union_player_app/utils/app_logger.dart';
 import 'package:union_player_app/utils/constants/constants.dart';
 import 'package:union_player_app/utils/dimensions/dimensions.dart';
 import 'package:union_player_app/utils/localizations/string_translation.dart';
-
+import 'package:webview_flutter/webview_flutter.dart';
 
 class FeedbackPage extends StatefulWidget {
   final AppLogger _logger;
@@ -47,66 +46,62 @@ class FeedbackPageState extends State {
     String _url = await _getUrlFromAssets();
     // For testing:
     // String _url = "https://stackoverflow.com/questions/52414629/how-to-update-state-of-a-modalbottomsheet-in-flutter";
-    int indexedStackPosition = 1 ;
+    int indexedStackPosition = 1;
 
     showModalBottomSheet(
         context: context,
         isScrollControlled: true,
         builder: (context) {
-          Set<Factory<OneSequenceGestureRecognizer>> _gestureRecognizers = [Factory(() => EagerGestureRecognizer())].toSet();
+          Set<Factory<OneSequenceGestureRecognizer>> _gestureRecognizers =
+              [Factory(() => EagerGestureRecognizer())].toSet();
           // Use StatefulBuilder in order to change state of bottomSheet:
           return StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState ) {
-                return
-                  IndexedStack(
-                      index: indexedStackPosition,
-                      children: <Widget>[
-                        WebView(
-                          key: _webViewKey,
-                          initialUrl: _url,
-                          javascriptMode: JavascriptMode.unrestricted,
-                          gestureRecognizers: _gestureRecognizers,
-                          onPageStarted: (value){
-                            _logger.logDebug("Loading about_page started...");
-                            setState(() {
-                              indexedStackPosition = 1;
-                            });},
-                          onPageFinished: (value) {
-                            _logger.logDebug("Loading about_page finished!");
-                            // Hide loading indicator:
-                            setState(() {
-                              indexedStackPosition = 0;
-                            });
-                          },
-                        ),
-                        Container(
-                          child: Center(
-                              child: CircularProgressIndicator()),
-                        ),
-                    ]);
-        });
+              builder: (BuildContext context, StateSetter setState) {
+            return IndexedStack(index: indexedStackPosition, children: <Widget>[
+              WebView(
+                key: _webViewKey,
+                initialUrl: _url,
+                javascriptMode: JavascriptMode.unrestricted,
+                gestureRecognizers: _gestureRecognizers,
+                onPageStarted: (value) {
+                  _logger.logDebug("Loading about_page started...");
+                  setState(() {
+                    indexedStackPosition = 1;
+                  });
+                },
+                onPageFinished: (value) {
+                  _logger.logDebug("Loading about_page finished!");
+                  // Hide loading indicator:
+                  setState(() {
+                    indexedStackPosition = 0;
+                  });
+                },
+              ),
+              Container(
+                child: Center(child: CircularProgressIndicator()),
+              ),
+            ]);
+          });
         });
   }
 
-  Future<String> _getUrlFromAssets()  async {
-    String fileText = await rootBundle.loadString('assets/about_us_page/test_page.html');
-    String url = Uri.dataFromString(
-    fileText,
-    mimeType: 'text/html',
-    encoding: Encoding.getByName('utf-8')).toString();
+  Future<String> _getUrlFromAssets() async {
+    String fileText =
+        await rootBundle.loadString('assets/about_us_page/test_page.html');
+    String url = Uri.dataFromString(fileText,
+            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        .toString();
     return url;
   }
 
-
-  _sendButtonPressed(){
+  _sendButtonPressed() {
     if (_formKey.currentState!.validate()) {
-      String text =
-      translate(StringKeys.formSuccessText, context);
+      String text = translate(StringKeys.formSuccessText, context);
       Color color = Colors.green;
       // Для использования Scaffold.of(context) я создала внутренний BuildContext - см. коммент выше,
       // т.к. аргумент context не может использоваться для поиска Scaffold, если он находится "выше" в дереве виджетов.
-      Scaffold.of(context).showSnackBar(SnackBar(
-          content: Text(text), backgroundColor: color));
+      Scaffold.of(context)
+          .showSnackBar(SnackBar(content: Text(text), backgroundColor: color));
     }
   }
 
@@ -168,24 +163,23 @@ class FeedbackPageState extends State {
                         ]))),
                 Padding(
                   padding: EdgeInsets.only(top: 15.h, right: 10.w),
-                  child:
-                     Row(
-                       mainAxisAlignment: MainAxisAlignment.center,
-                       children: [
-                         TextButton(
-                             onPressed: _aboutButtonPressed,
-                             child: Text(translate(StringKeys.aboutButtonText, context)),
-                         ),
-                         Padding(
-                           padding: EdgeInsets.only(left: 15.w),
-                           child:
-                           ElevatedButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: _aboutButtonPressed,
+                        child: Text(
+                            translate(StringKeys.aboutButtonText, context)),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(left: 15.w),
+                          child: ElevatedButton(
                             onPressed: _sendButtonPressed,
-                            child: Text(translate(StringKeys.sendButtonText, context)),
-                          )
-                         ),
-                      ],
-                     ),
+                            child: Text(
+                                translate(StringKeys.sendButtonText, context)),
+                          )),
+                    ],
+                  ),
                 ),
               ],
             )));
