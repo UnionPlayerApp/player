b5e919cba1d40ca582f1347f3183ce87f4bd0e1e
FedorSur <fyodor.s2013@yandex.ru>
2021-05-18 23:35:42 +0300

    Update settings_page.dart

--- lib/screen_settings/settings_page.dart
+++ lib/screen_settings/settings_page.dart
@@ -1,20 +1,119 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:union_player_app/utils/dimensions/dimensions.dart';
 
 class SettingsPage extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-      padding: EdgeInsets.only(top:40),
-      child: const Text(
-        "Settings",
-        style: TextStyle(
-            fontSize: 22,
-            color: Colors.lightBlue
+    return MaterialApp(
+      home: Scaffold(
+        body: const Center(
+          child: MyStatefulWidget(),
         ),
-        textAlign: TextAlign.center,
       ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValueTheme = 'Светлая тема';
+  String dropdownValueQuality = 'Среднее качество';
+  String dropdownValueAutoPlay = 'Воспроизведение при старте включено';
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        new Text("Тема",
+            style: TextStyle(fontSize: titleFontSize),
+            overflow: TextOverflow.ellipsis),
+      new DropdownButton<String>(
+      value: dropdownValueTheme,
+      icon: const Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValueTheme = newValue!;
+        });
+      },
+      items: <String>['Светлая тема', 'Тёмная тема', 'Системная тема']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    ),
+
+        new Text("Качество звука",
+            style: TextStyle(fontSize: titleFontSize),
+            overflow: TextOverflow.ellipsis),
+        new DropdownButton<String>(
+          value: dropdownValueQuality,
+          icon: const Icon(Icons.arrow_downward),
+          iconSize: 24,
+          elevation: 16,
+          style: const TextStyle(color: Colors.deepPurple),
+          underline: Container(
+            height: 2,
+            color: Colors.deepPurpleAccent,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValueQuality = newValue!;
+            });
+          },
+          items: <String>['Высокое качество', 'Среднее качество', 'Низкое качество', 'Авто']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+
+        new Text("Воспроизведение при старте программы",
+            style: TextStyle(fontSize: titleFontSize),
+            overflow: TextOverflow.ellipsis),
+        new DropdownButton<String>(
+          value: dropdownValueAutoPlay,
+          icon: const Icon(Icons.arrow_downward),
+          iconSize: 24,
+          elevation: 16,
+          style: const TextStyle(color: Colors.deepPurple),
+          underline: Container(
+            height: 2,
+            color: Colors.deepPurpleAccent,
+          ),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValueAutoPlay = newValue!;
+            });
+          },
+          items: <String>['Воспроизведение при старте включено', 'Воспроизведение при старте выключено']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
