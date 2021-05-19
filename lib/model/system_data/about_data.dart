516154df0ad425470dc38180a82b74b84d1e4896
Alexander Chepel <chepel.alexander@gmail.com>
2021-05-19 17:35:56 +0300

    develop: Fix critical bug because of firebase system data structure has changed

--- lib/model/system_data/about_data.dart
+++ lib/model/system_data/about_data.dart
@@ -1,18 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AboutData {
-  String textBy = "";
-  String textEn = "";
-  String textRu = "";
   String urlBy = "";
   String urlEn = "";
   String urlRu = "";
 
   void setData(DocumentSnapshot<Object?> doc) {
     try {
-      textBy = doc["text_by"];
-      textEn = doc["text_en"];
-      textRu = doc["text_ru"];
       urlBy = doc["url_by"];
       urlEn = doc["url_en"];
       urlRu = doc["url_ru"];
