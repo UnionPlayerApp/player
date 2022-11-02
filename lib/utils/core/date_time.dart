ca7b5ceb92a543c456ec9dcae8594d96eec0ee8b
chplAlex <chplalex.dev@gmail.com>
2022-11-02 21:48:08 +0200

    Fixed schedule algorithm.

--- lib/utils/core/date_time.dart
+++ lib/utils/core/date_time.dart
@@ -1,6 +1,8 @@
 import 'package:intl/intl.dart';
 import 'package:union_player_app/utils/core/relative_day_type.dart';
 
+const _moscowDiff = Duration(hours: -3);
+
 String formatDateTime(DateTime dateTime) {
   final type = getRelativeDayType(dateTime);
   final pattern = "${getRelativeDayPattern(type)}HH:mm";
@@ -19,7 +21,7 @@ DateTime parseDateTime(String date, String time) {
     final month = int.parse(dateParts[1]);
     final day = int.parse(dateParts[2]);
 
-    return DateTime(year, month, day, hours, minutes, seconds);
+    return DateTime.utc(year, month, day, hours, minutes, seconds).add(_moscowDiff).toLocal();
   } catch (error) {
     throw const FormatException('Invalid start date or time format');
   }
